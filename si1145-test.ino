b9402a9ac5e881eddef9b0346e4d6d92d8000811
JSX001 <jon.simcox@oppolis.com>
2016-05-09 15:55:08 +0100

    Create si1145-test.ino

---
+++ si1145-test.ino
@@ -0,0 +1,34 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "Adafruit_SI1145.h"
+
+
+Adafruit_SI1145 uv = Adafruit_SI1145();
+
+void setup() 
+{
+
+    Serial.begin(9600);
+}
+
+void loop() 
+{
+    // next line crashes with SOS
+    uv.readVisible();
+    
+    Serial.println("===================");
+    
+    
+//  Serial.print("Vis: "); Serial.println(uv.readVisible());
+//  Serial.print("IR: "); Serial.println(uv.readIR());
+
+/*
+  float UVindex = uv.readUV();
+  // the index is multiplied by 100 so to get the
+  // integer index, divide by 100!
+    UVindex /= 100.0;  
+  Serial.print("UV: ");  Serial.println(UVindex);
+*/
+
+  delay(750);
+
+}
